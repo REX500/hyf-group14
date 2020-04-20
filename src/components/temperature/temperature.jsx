5a9bceea751c12aebca1ef52824034dbf25ff92e
Filip Malek <filamalek@hotmail.com>
2020-04-20 22:12:16 +0200

    Added temperature template folder

---
+++ src/components/temperature/temperature.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+// components
+import Header from './components/dumb/header/header';
+
+// material
+import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import TextField from '@material-ui/core/TextField';
+
+function Temperature() {
+  return (
+    <div className="app">
+      <Header home>hjælpDeÆldre</Header>
+      {/* your code should start here :) */}
+      {/**
+       * how to use material-ui links are bellow
+       * - radio button: https://material-ui.com/components/radio-buttons/
+       * - input field https://material-ui.com/components/text-fields/
+       */}
+    </div>
+  );
+}
+
+export default Temperature;
